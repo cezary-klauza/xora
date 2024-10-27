a50adc8f1cf02715bec9964fb1ebbbfa11238978
Cezary Klauza <cezary10klauza@gmail.com>
2024-10-27 20:25:01 +0100

    created pricing section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import Features from './sections/Features'
 import Header from './sections/Header'
 import Hero from './sections/Hero'
+import Pricing from './sections/Pricing'
 
 function App() {
 
@@ -7,6 +9,8 @@ function App() {
     <main className='overflow-hidden'>
       <Header/>
       <Hero />
+      <Features />
+      <Pricing />
     </main>
   )
 }

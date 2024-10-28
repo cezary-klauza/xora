97b0e919e6bd83e046ce276b3417e48fe39bfb9b
Cezary Klauza <cezary10klauza@gmail.com>
2024-10-28 17:40:55 +0100

    reapaired build bugs v2

--- src/components/TestimonialItem.tsx
+++ src/components/TestimonialItem.tsx
@@ -8,15 +8,15 @@ type Props = {
     avatarUrl: string;
     comment: string;
   };
-  containerClassname: string;
+  containerClassName: string;
 };
 
-const TestimonialItem = ({ item, containerClassname }: Props) => {
+const TestimonialItem = ({ item, containerClassName }: Props) => {
   return (
     <div
       className={clsx(
         "relative px-14 pt-11 pb-14 after:absolute after:bottom-0 after:right-0 after:h-0.5 after:w-screen after:bg-s2 after:content-[''] max-md:px-0 max-md:pt-11 after:max-md:-right-4",
-        containerClassname
+        containerClassName
       )}
     >
       <blockquote className="h6 mb-8 text-p4">{item.comment}</blockquote>
